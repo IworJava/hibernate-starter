95c1b9a179997038ac5ccb1b3c1329e9fe6753d0
Iw <iwor.java@gmail.com>
2023-06-20 15:37:14 +0400

    add Hibernate Runner

---
+++ src/main/java/com/iwor/HibernateRunner.java
@@ -0,0 +1,37 @@
+package com.iwor;
+
+import com.iwor.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
+
+public class HibernateRunner {
+
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()
+        ) {
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 19))
+                    .age(23)
+                    .build();
+
+            session.delete(user);
+            session.save(user);
+
+            session.getTransaction().commit();
+        }
+    }
+}
