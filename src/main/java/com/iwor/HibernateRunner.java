c2aea04d9eb18b40a1251ea3dd53326061a4d5b8
Iw <iwor.java@gmail.com>
2023-06-23 13:10:05 +0400

    add logging

--- src/main/java/com/iwor/HibernateRunner.java
+++ src/main/java/com/iwor/HibernateRunner.java
@@ -3,45 +3,49 @@ package com.iwor;
 import com.iwor.entity.Birthday;
 import com.iwor.entity.Role;
 import com.iwor.entity.User;
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.iwor.util.HibernateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
 
 import java.time.LocalDate;
 
+@Slf4j
 public class HibernateRunner {
+    private static final User USER = User.builder()
+            .username("ivan1@gmail.com")
+            .firstname("Ivan")
+            .lastname("Ivanov")
+            .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
+            .role(Role.USER)
+            .info("""
+                    {
+                        "name": "Ivan",
+                        "id": 25
+                    }
+                    """)
+            .build();
 
     public static void main(String[] args) {
-        Configuration configuration = new Configuration();
-        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
-        configuration.registerTypeOverride(new JsonBinaryType());
-        configuration.configure();
+        log.info("User entity is in the transient state, object: {}", USER);
 
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
-             Session session = sessionFactory.openSession()
-        ) {
-            session.beginTransaction();
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            try (session) {
+                Transaction transaction = session.beginTransaction();
+                log.trace("Transaction is created, transaction: {}", transaction);
 
-            User user = User.builder()
-                    .username("ivan1@gmail.com")
-                    .firstname("Ivan")
-                    .lastname("Ivanov")
-                    .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
-                    .role(Role.USER)
-                    .info("""
-                            {
-                                "name": "Ivan",
-                                "id": 25
-                            }
-                            """)
-                    .build();
+//                session.merge(USER);
+                log.trace("User is in the persistent state: {}, session: {}", USER, session);
 
-            session.delete(user);
-            session.save(user);
+                session.getTransaction().commit();
+            }
+            log.warn("User is in the detached state: {}, session is closed: {}", USER, session);
 
-            session.getTransaction().commit();
+        } catch (Exception e) {
+            log.error("An exception occurred", e);
+            throw e;
         }
     }
 }
---
+++ src/main/java/com/iwor/util/HibernateUtil.java
@@ -0,0 +1,19 @@
+package com.iwor.util;
+
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import lombok.experimental.UtilityClass;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.cfg.Configuration;
+
+@UtilityClass
+public class HibernateUtil {
+
+    public static SessionFactory buildSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        configuration.registerTypeOverride(new JsonBinaryType());
+        configuration.configure();
+        return configuration.buildSessionFactory();
+    }
+}
