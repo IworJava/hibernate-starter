04260a515638837a6d442dbd636e82a8e7a87e1a
Iw <iwor.java@gmail.com>
2023-06-26 12:50:54 +0400

    add ManyToOne

--- src/main/java/com/iwor/HibernateRunner.java
+++ src/main/java/com/iwor/HibernateRunner.java
@@ -1,6 +1,7 @@
 package com.iwor;
 
 import com.iwor.entity.Birthday;
+import com.iwor.entity.Company;
 import com.iwor.entity.PersonalInfo;
 import com.iwor.entity.Role;
 import com.iwor.entity.User;
@@ -14,50 +15,34 @@ import java.time.LocalDate;
 
 @Slf4j
 public class HibernateRunner {
+    private static final Company COMPANY = new Company(null, "Google");
     private static final PersonalInfo PERSONAL_INFO = PersonalInfo.builder()
-            .firstname("Ivan")
-            .lastname("Ivanov")
+            .firstname("Petr")
+            .lastname("Petrov")
             .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
             .build();
     private static final User USER = User.builder()
-//            .id(2L)
-            .username("ivan@gmail.com")
+            .username("petr1@gmail.com")
             .personalInfo(PERSONAL_INFO)
             .role(Role.USER)
             .info("{\"id\": 25, \"name\": \"Ivan\"}")
+//            .company(COMPANY)
             .build();
 
     public static void main(String[] args) {
-        log.info("User entity is in the transient state, object: {}", USER);
-
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
             Session session = sessionFactory.openSession();
             try (session) {
                 Transaction transaction = session.beginTransaction();
-                log.trace("Transaction is created, transaction: {}", transaction);
 
-                session.save(USER);
-                log.trace("User is in the persistent state: {}, session: {}", USER, session);
+//                session.save(COMPANY);
+//                session.save(USER);
 
-                session.getTransaction().commit();
+                User user = session.get(User.class, 2L);
+                System.out.println(user);
+
+                transaction.commit();
             }
-            log.warn("User is in the detached state: {}, session is closed: {}", USER, session);
-
-            Session session1 = sessionFactory.openSession();
-            try (session1) {
-                Transaction transaction = session1.beginTransaction();
-                log.trace("Transaction is created, transaction: {}", transaction);
-
-                User user = session1.get(User.class, PERSONAL_INFO);
-                log.trace("User is in the persistent state: {}, session: {}", USER, session1);
-
-                session1.getTransaction().commit();
-            }
-            log.warn("User is in the detached state: {}, session is closed: {}", USER, session1);
-
-        } catch (Exception e) {
-            log.error("An exception occurred", e);
-            throw e;
         }
     }
 }
--- src/main/java/com/iwor/entity/User.java
+++ src/main/java/com/iwor/entity/User.java
@@ -6,39 +6,53 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
-import javax.persistence.EmbeddedId;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "company")
+@Builder
 @Entity
 @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 @Table(name = "users", schema = "public")
 public class User {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 //    @GeneratedValue(generator = "user_gen", strategy = GenerationType.SEQUENCE)
 //    @SequenceGenerator(name = "user_gen", sequenceName = "users_id_seq", allocationSize = 1)
-//    private Long id;
+    private Long id;
+
     @Column(unique = true)
     private String username;
-    @EmbeddedId
+
+    @Embedded
 //    @AttributeOverride(name = "birthdate", column = @Column(name = "birth_date"))
     private PersonalInfo personalInfo;
+
     @Convert(converter = RoleConverter.class)
     private Role role;
+
     @Type(type = "jsonb")
     private String info;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    private Company company;
 }
+
