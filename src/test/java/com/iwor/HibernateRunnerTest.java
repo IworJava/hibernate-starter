fb5a274d2e368320b6e06f53a5e9c1689583c8a4
Iw <iwor.java@gmail.com>
2023-06-28 18:59:05 +0400

    add Docker Test Containers

--- src/test/java/com/iwor/HibernateRunnerTest.java
+++ src/test/java/com/iwor/HibernateRunnerTest.java
@@ -3,8 +3,10 @@ package com.iwor;
 import com.iwor.entity.Chat;
 import com.iwor.entity.Company;
 import com.iwor.entity.Profile;
+import com.iwor.entity.Role;
 import com.iwor.entity.User;
 import com.iwor.entity.UserChat;
+import com.iwor.util.HibernateTestUtil;
 import com.iwor.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -18,46 +20,6 @@ import org.junit.jupiter.api.Test;
 import java.time.Instant;
 
 class HibernateRunnerTest {
-    private static final String DROP_ALL_SQL = """
-            DROP TABLE IF EXISTS profile,
-                                 users,
-                                 company_locale,
-                                 company;
-            """;
-    private static final String CREATE_COMPANY_SQL = """
-            CREATE TABLE company (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(64) NOT NULL UNIQUE
-            );
-            """;
-    private static final String CREATE_COMPANY_LOCALE_SQL = """
-            CREATE TABLE company_locale (
-                company_id INT NOT NULL REFERENCES company (id),
-                lang CHAR(2) NOT NULL,
-                description VARCHAR(128) NOT NULL,
-                PRIMARY KEY (company_id, lang)
-            );
-            """;
-    private static final String CREATE_PROFILE_SQL = """
-            CREATE TABLE profile (
-                id BIGSERIAL PRIMARY KEY,
-                street VARCHAR(128),
-                language CHAR(2)
-            );
-            """;
-    private static final String CREATE_USERS_SQL = """
-            CREATE TABLE users (
-                id BIGSERIAL PRIMARY KEY,
-                username VARCHAR(128) NOT NULL UNIQUE,
-                firstname VARCHAR(128),
-                lastname VARCHAR(128),
-                birth_date DATE,
-                role VARCHAR(1),
-                info JSONB,
-                company_id INT NULL REFERENCES company (id),
-                profile_id BIGINT UNIQUE REFERENCES profile (id)
-            );
-            """;
     private static SessionFactory sessionFactory;
     private Session session;
 
@@ -131,18 +93,15 @@ class HibernateRunnerTest {
 
     @BeforeAll
     static void createSessionFactory() {
-        sessionFactory = HibernateUtil.buildSessionFactory();
+        sessionFactory = HibernateTestUtil.buildSessionFactory();
     }
     @BeforeEach
     void initDb() {
         session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
 
-        session.createSQLQuery(DROP_ALL_SQL).executeUpdate();
-        session.createSQLQuery(CREATE_COMPANY_SQL).executeUpdate();
-        session.createSQLQuery(CREATE_COMPANY_LOCALE_SQL).executeUpdate();
-        session.createSQLQuery(CREATE_PROFILE_SQL).executeUpdate();
-        session.createSQLQuery(CREATE_USERS_SQL).executeUpdate();
+//        Arrays.stream(SqlQueries.values()).forEachOrdered(sql ->
+//                session.createSQLQuery(sql.getQuery()).executeUpdate());
 
         Company company1 = Company.builder().name("Apple").build();
         Company company2 = Company.builder().name("Google").build();
@@ -151,9 +110,9 @@ class HibernateRunnerTest {
         session.persist(company2);
         session.persist(company3);
 
-        User user1 = User.builder().username("ivan@gmail.com").company(company1).build();
-        User user2 = User.builder().username("petr@gmail.com").company(company1).build();
-        User user3 = User.builder().username("anna@gmail.com").company(company1).build();
+        User user1 = User.builder().username("ivan@gmail.com").role(Role.USER).company(company1).build();
+        User user2 = User.builder().username("petr@gmail.com").role(Role.USER).company(company1).build();
+        User user3 = User.builder().username("anna@gmail.com").role(Role.ADMIN).company(company1).build();
         session.persist(user1);
         session.persist(user2);
         session.persist(user3);
